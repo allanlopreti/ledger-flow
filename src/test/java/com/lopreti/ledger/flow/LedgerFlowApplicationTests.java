2895fbcd74a1a9ca0e6b9d6d30c94d693a558368
Rafael Lopreti <rafael.lopreti@gmail.com>
2026-08-08 19:02:12 -0300

    feat: initialize ledger application

---
+++ src/test/java/com/lopreti/ledger/flow/LedgerFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lopreti.ledger.flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
